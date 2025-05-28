b825a90e1be793df96a73464b12e5749d7df772f
NateFLinux424 <fagernessn@gmail.com>
2025-05-28 13:54:27 -0700

    Update kickstarter.sh

--- kickstarter.sh
+++ kickstarter.sh
@@ -7,6 +7,10 @@ sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+# Add the current user to the Docker group (avoiding permission issues) 
+sudo groupadd docker
+sudo usermod -aG docker $USER newgrp docker
+
 # Add the Docker repository to Apt sources
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
